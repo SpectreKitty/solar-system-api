c3f0dcce60062898968f0a6e83bf0381e2d99a56
AstryBrana <astry.brana@gmail.com>
2024-10-21 14:25:12 -0400

    added wave 2, get one planet with error handling

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, abort, make_response
 from app.models.planets import planets
 
 planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
@@ -16,3 +16,28 @@ def get_all_planets():
         ))
     
     return response_list
+
+@planets_bp.get("<planet_id>")
+def get_one_planet(planet_id):
+    planet = validate_planet(planet_id)
+
+    return dict(
+        id=planet.id,
+        name=planet.name,
+        description=planet.description,
+        orbit=planet.orbit
+            )
+
+def validate_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except ValueError:
+        response = {"message": f"Planet {planet_id} is invalid"}
+        abort(make_response(response, 400))
+
+    for planet in planets:
+        if planet.id == planet_id:
+            return planet
+
+    response = {"message":f"Planet {planet_id} not found"}
+    abort(make_response(response, 404))
