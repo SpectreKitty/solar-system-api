2608a9a1b440a43d80c557df87cb85efe15b37a1
Jenny Massari <jennymartins13@gmail.com>
2024-10-30 11:39:43 -0700

    Added tests for wave 6.

--- app/__init__.py
+++ app/__init__.py
@@ -2,17 +2,24 @@ from flask import Flask
 from .db import db, migrate
 from .models import planets
 from app.routes.routes import planets_bp
+import os 
 
 
-def create_app(test_config=None):
+def create_app(config=None):
     app = Flask(__name__)
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+
+    if config:
+        # Merge `config` into the app's configuration
+        # to override the app's default settings
+        app.config.update(config)
 
     db.init_app(app)
     migrate.init_app(app, db)
 
+    # Register Blueprints here
     app.register_blueprint(planets_bp)
 
     return app
